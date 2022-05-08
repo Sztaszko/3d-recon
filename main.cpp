472e1c331ba9fe6a0d95edcdf291a7c7c382dee2
Sztaszko <majneimejl@gmail.com>
2022-05-08 23:36:08 +0200

    Update with other tutorial code with separated calibration and step-by-step reconstruction.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@
 #include <opencv2/core.hpp>
 #include <iostream>
 #include <fstream>
+#include "reconstruction.h"
+#include "cameracalibrator.h"
+
 using namespace std;
 using namespace cv;
 using namespace cv::sfm;
+
+std::vector<std::vector<cv::Point3f>> objectPoints;
+std::vector<std::vector<cv::Point2f>> imagePoints;
+// output Matrices
+cv::Mat cameraMatrix;
+cv::Mat distCoeffs;
+
 static void help() {
   cout
       << "\n------------------------------------------------------------------------------------\n"
@@ -39,75 +49,123 @@ static int getdir(const string _filename, vector<String> &files)
   }
   return 1;
 }
-int main(int argc, char* argv[])
-{
-  // Read input parameters
-  if ( argc != 5 )
-  {
-    help();
-    exit(0);
+
+
+
+int main(){
+
+  cout<<"compiled"<<endl;
+
+  const std::vector<std::string> files = {"boards/1.jpg", "boards/2.jpg","boards/3.jpg","boards/4.jpg","boards/5.jpg","boards/6.jpg","boards/7.jpg","boards/8.jpg","boards/9.jpg","boards/10.jpg","boards/11.jpg","boards/12.jpg","boards/13.jpg","boards/14.jpg","boards/15.jpg","boards/16.jpg","boards/17.jpg","boards/18.jpg","boards/19.jpg","boards/20.jpg","boards/21.jpg","boards/22.jpg","boards/23.jpg","boards/24.jpg","boards/25.jpg"};
+  cv::Size board_size(7,7);
+
+  CameraCalibrator cal;
+  cal.addChessboardPoints(files, board_size);
+
+  cv::Mat img = cv::imread("boards/1.jpg");
+
+  cv::Size img_size = img.size();
+  cal.calibrate(img_size);
+  cout<<cameraMatrix<<endl;
+
+
+  cv::Mat image1 = cv::imread("imR.png");
+  cv::Mat image2 = cv::imread("imL.png");
+
+  // vector of keypoints and descriptors
+  std::vector<cv::KeyPoint> keypoints1;
+  std::vector<cv::KeyPoint> keypoints2;
+  cv::Mat descriptors1, descriptors2;
+
+  // Construction of the SIFT feature detector
+  cv::Ptr<cv::Feature2D> ptrFeature2D = cv::xfeatures2d::SIFT::create(10000);
+
+  // Detection of the SIFT features and associated descriptors
+  ptrFeature2D->detectAndCompute(image1, cv::noArray(), keypoints1, descriptors1);
+  ptrFeature2D->detectAndCompute(image2, cv::noArray(), keypoints2, descriptors2);
+
+  // Match the two image descriptors
+  // Construction of the matcher with crosscheck
+  cv::BFMatcher matcher(cv::NORM_L2, true);
+  std::vector<cv::DMatch> matches;
+  matcher.match(descriptors1, descriptors2, matches);
+
+  cv::Mat matchImage;
+
+
+  cv::namedWindow("img1");
+  cv::drawMatches(image1, keypoints1, image2, keypoints2, matches, matchImage, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
+  cv::imwrite("matches.jpg", matchImage);
+
+  // Convert keypoints into Point2f
+  std::vector<cv::Point2f> points1, points2;
+
+  for (std::vector<cv::DMatch>::const_iterator it = matches.begin(); it != matches.end(); ++it) {
+    // Get the position of left keypoints
+    float x = keypoints1[it->queryIdx].pt.x;
+    float y = keypoints1[it->queryIdx].pt.y;
+    points1.push_back(cv::Point2f(x, y));
+    // Get the position of right keypoints
+    x = keypoints2[it->trainIdx].pt.x;
+    y = keypoints2[it->trainIdx].pt.y;
+    points2.push_back(cv::Point2f(x, y));
   }
-  // Parse the image paths
-  vector<String> images_paths;
-  getdir( argv[1], images_paths );
-  // Build intrinsics
-  float f  = atof(argv[2]),
-        cx = atof(argv[3]), cy = atof(argv[4]);
-  Matx33d K = Matx33d( f, 0, cx,
-                       0, f, cy,
-                       0, 0,  1);
-  bool is_projective = true;
-  vector<Mat> Rs_est, ts_est, points3d_estimated;
-  reconstruct(images_paths, Rs_est, ts_est, K, points3d_estimated, is_projective);
-  // Print output
-  cout << "\n----------------------------\n" << endl;
-  cout << "Reconstruction: " << endl;
-  cout << "============================" << endl;
-  cout << "Estimated 3D points: " << points3d_estimated.size() << endl;
-  cout << "Estimated cameras: " << Rs_est.size() << endl;
-  cout << "Refined intrinsics: " << endl << K << endl << endl;
-  cout << "3D Visualization: " << endl;
-  cout << "============================" << endl;
-  viz::Viz3d window("Coordinate Frame");
-             window.setWindowSize(Size(500,500));
-             window.setWindowPosition(Point(150,150));
-             window.setBackgroundColor(); // black by default
-  // Create the pointcloud
-  cout << "Recovering points  ... ";
-  // recover estimated points3d
-  vector<Vec3f> point_cloud_est;
-  for (int i = 0; i < points3d_estimated.size(); ++i)
-    point_cloud_est.push_back(Vec3f(points3d_estimated[i]));
-  cout << "[DONE]" << endl;
-  cout << "Recovering cameras ... ";
-  vector<Affine3d> path;
-  for (size_t i = 0; i < Rs_est.size(); ++i)
-    path.push_back(Affine3d(Rs_est[i],ts_est[i]));
-  cout << "[DONE]" << endl;
-  if ( point_cloud_est.size() > 0 )
-  {
-    cout << "Rendering points   ... ";
-    viz::WCloud cloud_widget(point_cloud_est, viz::Color::green());
-    window.showWidget("point_cloud", cloud_widget);
-    cout << "[DONE]" << endl;
+
+  // Find the essential between image 1 and image 2
+  cv::Mat inliers;
+  cv::Mat essential = cv::findEssentialMat(points1, points2, cameraMatrix, cv::RANSAC, 0.9, 1.0, inliers);
+
+  cout<<essential<<endl;
+
+  // recover relative camera pose from essential matrix
+  cv::Mat rotation, translation;
+  cv::recoverPose(essential, points1, points2, cameraMatrix, rotation, translation, inliers);
+  cout<<rotation<<endl;
+  cout<<translation<<endl;
+
+
+  // compose projection matrix from R,T
+  cv::Mat projection2(3, 4, CV_64F); // the 3x4 projection matrix
+  rotation.copyTo(projection2(cv::Rect(0, 0, 3, 3)));
+  translation.copyTo(projection2.colRange(3, 4));
+  // compose generic projection matrix
+  cv::Mat projection1(3, 4, CV_64F, 0.); // the 3x4 projection matrix
+  cv::Mat diag(cv::Mat::eye(3, 3, CV_64F));
+  diag.copyTo(projection1(cv::Rect(0, 0, 3, 3)));
+  // to contain the inliers
+  std::vector<cv::Vec2d> inlierPts1;
+  std::vector<cv::Vec2d> inlierPts2;
+  // create inliers input point vector for triangulation
+  int j(0);
+  for (int i = 0; i < inliers.rows; i++) {
+    if (inliers.at<uchar>(i)) {
+      inlierPts1.push_back(cv::Vec2d(points1[i].x, points1[i].y));
+      inlierPts2.push_back(cv::Vec2d(points2[i].x, points2[i].y));
+    }
   }
-  else
-  {
-    cout << "Cannot render points: Empty pointcloud" << endl;
-  }
-  if ( path.size() > 0 )
-  {
-    cout << "Rendering Cameras  ... ";
-    window.showWidget("cameras_frames_and_lines", viz::WTrajectory(path, viz::WTrajectory::BOTH, 0.1, viz::Color::green()));
-    window.showWidget("cameras_frustums", viz::WTrajectoryFrustums(path, K, 0.1, viz::Color::yellow()));
-    window.setViewerPose(path[0]);
-    cout << "[DONE]" << endl;
-  }
-  else
-  {
-    cout << "Cannot render the cameras: Empty path" << endl;
-  }
-  cout << endl << "Press 'q' to close each windows ... " << endl;
+  // undistort and normalize the image points
+  std::vector<cv::Vec2d> points1u;
+  cv::undistortPoints(inlierPts1, points1u, cameraMatrix, distCoeffs);
+  std::vector<cv::Vec2d> points2u;
+  cv::undistortPoints(inlierPts2, points2u, cameraMatrix, distCoeffs);
+
+
+  // Triangulation
+  std::vector<cv::Vec3d> points3D;
+  cal.triangulate(projection1, projection2, points1u, points2u, points3D);
+
+  cout<<"3D points :"<<points3D.size()<<endl;
+
+  viz::Viz3d window; //creating a Viz window
+
+  //Displaying the Coordinate Origin (0,0,0)
+  window.showWidget("coordinate", viz::WCoordinateSystem());
+
+  window.setBackgroundColor(cv::viz::Color::black());
+
+  //Displaying the 3D points in green
+  window.showWidget("points", viz::WCloud(points3D, viz::Color::green()));
   window.spin();
-  return 0;
+
+
 }
