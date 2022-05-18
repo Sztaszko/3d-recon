7ff170f87be9dd6828814a415ce01eadeca71073
Sztaszko <majneimejl@gmail.com>
2022-05-18 20:30:01 +0200

    Modify move method and derive from VideoCapture.

--- inc/camera.h
+++ inc/camera.h
@@ -2,16 +2,23 @@
 #define CAMERA_H
 
 #include <vector>
+#include <opencv2/videoio.hpp>
 
 namespace cameraAPI {
 
 /*!
  * \brief The Camera class - base camera class for camera movement API
  */
-class Camera {
+class Camera : public cv::VideoCapture {
 
 public:
     Camera();
+
+    /*!
+     * \brief init - initializes camera position based on given coordinates by calibration
+     * \param position - vector of position containing {_posX, _posY, _posZ, _roll, _pitch, _yaw};
+     */
+    virtual void init(std::vector<double> position);
 
     /*!
      * \brief move - moves camera to given position
@@ -27,22 +34,42 @@ public:
     double get_posY() { return _posY; }
     double get_posZ() { return _posZ; }
 
+    double get_pitch() { return _pitch; }
+    double get_roll() { return _roll; }
+    double get_yaw() { return _yaw; }
+
+    virtual std::vector<double> get_position() { return {_posX, _posY, _posZ, _roll, _pitch, _yaw}; }
+
 
 protected:
-
     double _posX, _posY, _posZ;
+
+    double _pitch, _roll, _yaw; //radians
 
 };
 
 
 class HandCamera : public Camera {
 
+/* Assuming camera movement is straight line
+ *
+ *          OBJECT
+ *
+ *          -->
+ *  ----U-----------------------
+ *  (camera)
+ *
+ *  So giving and getting the camera position is done by hand,
+ *  only in one dimension. So main position change is translation with
+ *  slight rotation also in one dimension. Although there is possibility
+ *  that rotation in future will be calculated based on indicator in the scene.
+ *
+ */
+
 public:
     HandCamera();
 
     std::vector<double> move(double x, double y, double z) override;
-
-    std::vector<double> move(int alpha);
 
 };
 
--- src/camera.cpp
+++ src/camera.cpp
@@ -4,44 +4,51 @@
 
 const double PI = 3.14159265;
 
+
+cameraAPI::Camera::Camera() :
+    cv::VideoCapture()
+  , _posX(0)
+  , _posY(0)
+  , _posZ(0)
+  , _roll(0)
+  , _pitch(0)
+  , _yaw(0)
+{
+
+}
+
+void cameraAPI::Camera::init(std::vector<double> position)
+{
+    try {
+        _posX = position.at(0);
+        _posY = position.at(1);
+        _posZ = position.at(2);
+        _roll = position.at(3);
+        _pitch = position.at(4);
+        _yaw = position.at(5);
+    } catch (std::out_of_range &err) {
+        std::cerr << "Init vector error: " << err.what() << "\n";
+        std::cout << "Camera position assumed at: " << _posX, _posY, _posZ, _roll, _pitch, _yaw;
+    }
+}
+
+
 cameraAPI::HandCamera::HandCamera() :
     Camera()
 {
-
 }
 
 std::vector<double> cameraAPI::HandCamera::move(double x, double y, double z)
 {
-    // assume radius is 1 m and object is in 0,0,0 position
-    // we move only in XZ plane, not changing Y
-
-
-    std::cout << "Camera move requested. Please ";
-}
-
-std::vector<double> cameraAPI::HandCamera::move(int alpha)
-{
-    //assume moving only in XZ plane by the circle with radius of 1 m
-
     int input;
-
-    std::cout << "Camera move requested. Please rotate disc by " << alpha << " degrees.";
-    std::cout << "Type in the real roteted value: ";
+    std::cout << "Camera move requested. Please move camera by " << _posX - x << " m";
+    std::cout << "Input actual translated value: ";
     std::cin >> input;
 
+    _posX += input;
 
-    double angle_rad = (PI*input)/180;
+    // TODO rotation - get from user or calculate from images
 
-    _posX += sin(angle_rad);
-    _posZ += cos(angle_rad);
-
-    return { _posX, _posY, _posZ };
+    return { _posX, _posY, _posZ, _roll, _pitch, _yaw };
 }
 
-cameraAPI::Camera::Camera() :
-    _posX(0)
-  , _posY(0)
-  , _posZ(0)
-{
-
-}
